171820abec1344459e210d7b4d9272e0466e1728
Reza <108584275+rezamovaheddi@users.noreply.github.com>
2024-04-10 18:39:58 +0330

    add links on card

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,36 @@ function App() {
           <h1 className="font-bold text-3xl">Reza</h1>
           <p className="text-gray-600">Front-end Developer</p>
         </div>
-
+        <div className="card-items flex justify-between bg-gray-300 px-5 py-4 rounded-full mb-5 hover:bg-blue-500 hover:scale-105 duration-500 cursor-pointer hover:font-semibold">
+          <p className="name hover:scale-105 duration-500 ">Github</p>
+          <div className="icon">
+            <i className="fa-brands fa-github"></i>
+          </div>
+        </div>
+        <div className="card-items flex justify-between bg-gray-300 px-5 py-4 rounded-full mb-5 hover:bg-blue-500 hover:scale-105 duration-500 cursor-pointer hover:font-semibold">
+          <p className="name hover:scale-105 duration-500 ">Twitter</p>
+          <div className="icon">
+            <i className="fa-brands fa-twitter"></i>
+          </div>
+        </div>
+        <div className="card-items flex justify-between bg-gray-300 px-5 py-4 rounded-full mb-5 hover:bg-blue-500 hover:scale-105 duration-500 cursor-pointer hover:font-semibold">
+          <p className="name hover:scale-105 duration-500 ">Youtube</p>
+          <div className="icon">
+            <i className="fa-brands fa-youtube"></i>
+          </div>
+        </div>
+        <div className="card-items flex justify-between bg-gray-300 px-5 py-4 rounded-full mb-5 hover:bg-blue-500 hover:scale-105 duration-500 cursor-pointer hover:font-semibold">
+          <p className="name hover:scale-105 duration-500 ">Telegram</p>
+          <div className="icon">
+            <i className="fa-brands fa-telegram"></i>
+          </div>
+        </div>
+        <div className="card-items flex justify-between bg-gray-300 px-5 py-4 rounded-full mb-5 hover:bg-blue-500 hover:scale-105 duration-500 cursor-pointer hover:font-semibold">
+          <p className="name hover:scale-105 duration-500 ">LinkrdIn</p>
+          <div className="icon">
+            <i className="fa-brands fa-linkedin"></i>
+          </div>
+        </div>
       </div>
     </div>
     </>
