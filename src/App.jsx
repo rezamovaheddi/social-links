28edfbcc8c62aa72bf70279f382db92c87bc43be
Reza <108584275+rezamovaheddi@users.noreply.github.com>
2024-04-10 18:38:54 +0330

    change and main social- card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+    <div className="flex justify-center items-center h-screen">
+      <div className=" bg-white p-10 rounded-lg w-[30%] shadow-2xl">
+        <div className="card-header text-center pb-5">
+          <img src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/279.jpg" 
+          alt="XXX"
+          className="rounded-full m-auto shadow-md" />
+          <h1 className="font-bold text-3xl">Reza</h1>
+          <p className="text-gray-600">Front-end Developer</p>
+        </div>
 
+      </div>
+    </div>
     </>
   )
 }
